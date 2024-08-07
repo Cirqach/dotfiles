288a0e0a2dd70e940c6ece3232d61ef3accc8465
Cirqach <unamed@duck.com>
2024-08-07 13:10:24 +0300

    add some changes

--- nvim/lua/plugins/nvim-dap-virtual-text.lua
+++
@@ -1,8 +0,0 @@
-return {
-  "mfussenegger/nvim-dap",
-  dependencies = { -- optional packages
-    "theHamsta/nvim-dap-virtual-text",
-    "nvim-neotest/nvim-nio",
-    "rcarriga/nvim-dap-ui",
-  },
-}
